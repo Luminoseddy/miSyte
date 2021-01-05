fd0126faccc3cc8974033803182c99901b7477fe
Edward Palermo <eddx540@gmail.com>
2021-01-05 14:19:30 -0500

    Installed joi, 2 validations, Created Error class
    Implementations:
    - Created Error class
    - Error page in a red box occurs when invalid url is trying to be access.
    -  Defined a  Schema
    
    - Validation on client side
    - Validation on server side.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate_Engine = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utilities/catchAsync');
 const methodOverride = require('method-override'); // from Express
 const ExpressError = require('./utilities/catchAsync');
@@ -49,16 +50,29 @@ app.get('/campspots/new', (req, res) => {
     res.render('campspots/new');
 })
 
-app.post('/campspots', catchAsync(async(req, res) => {
-    if(!req.body.campspot) throw new ExpressError('Invalid camp spot data.', 404); // Infrastructure set up. 
+app.post('/campspots', catchAsync(async(req, res, next) => {
+    // if(!req.body.campspot) throw new ExpressError('Invalid camp spot data.', 404); // Infrastructure set up. 
+
+    // SCHEMA DEFINED
+    const campspotSchema = Joi.object({
+        campspot: Joi.object().required()({
+            title: Joi.string().required(),
+            price: Joi.number().required.min(0),
+            image: Joi.string().required(),
+            location: Joi.string.required(),
+            description: Joi.string().required()
+        }).required()
+    }) 
+    const { error } = campspotSchema.validate(req.body);
+    if(error){ // then we map over 
+        const msg = error.details.map(el = el.message).join(',');
+        throw new ExpressError(msg, 400); // When caught, gets thrown to app.use(a, b, c, next);
+    }
+    console.log(result);
     const campspot = new Campspot(req.body.campspot); // empty by default.
     await campspot.save();
     res.redirect(`/campspots/${campspot._id}`);  
-}))
-
-
-
-
+}));
 
 // Show page
 app.get('/campspots/:id', catchAsync(async(req, res,) => {
@@ -70,11 +84,6 @@ app.get('/campspots/:id/edit', catchAsync(async (req, res) => {
     const campspot = await Campspot.findById(req.params.id);
     res.render('campspots/edit', { campspot }); // take 'campspot' and pass it down to /edit
 }))
-
-
-
-
-
 
 app.put('/campspots/:id', catchAsync(async (req, res) => {
     // res.send("Testing app.put request /:id")
@@ -106,14 +115,13 @@ app.delete('/campspots/:id', catchAsync(async (req, res) => {
 // Only runs if nothing else was matched first.
 // This error handler uses next() and calls the next function
 app.all('*', (req, res, next) => {
-    next(new ExpressError('For some reason... This page can not be seen at the moment', 404));
+    next(new ExpressError('PAGE not found', 404));
 })
 
 app.use((err, req, res, next) =>{
-    // Destructor from err. 
+    // Destructor from err. extracting a variable from err, and giving the variable a default.
     const { statusCode=500, message='Shits going down.' } = err;
-    res.status(statusCode).send(message)
-    res.send('uugghh, something went wrong');
+    res.status(statusCode).render('error', { err} )
 })
 
 app.listen(3000, () => {
