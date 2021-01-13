ac1bcd0d4c46b922ea7a36a540db4353eea24e50
Edward Palermo <eddx540@gmail.com>
2021-01-13 10:17:31 -0500

    Model updated: author
    Implementations:
    - author added to the model campspots.
    -
    
    S52V514

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,43 +1,53 @@
 const express = require('express');
-const router = express.Router({ mergeParams: true });
+const router = express.Router({mergeParams: true});
 
 const Campspot = require('../models/campspot');
 const Review = require('../models/review');
 
 const { reviewSchema } = require('../schemas.js');
 
-
 const ExpressError = require('../utilities/ExpressError');
 const catchAsync = require('../utilities/catchAsync');
 
+
+
 const validateReview = (req, res, next) => {
+    // validate with req.body.
     const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
+    if(error){ // then we map over over every error.detail message.
+        const msg = error.details.map(el = el.message).join(',');
+        throw new ExpressError(msg, 400); // When caught, gets thrown to app.use(a, b, c, next);
+    }else{
         next();
     }
 }
 
 
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+
+
+router.post('/', validateReview, catchAsync(async(req, res) => {
+    // res.send("We made it, post request succeeded.")
     const campspot = await Campspot.findById(req.params.id);
     const review = new Review(req.body.review);
-    campspot.reviews.push(review);
+    campspot.reviews.push(review); //recall reviews property from campspot.js
     await review.save();
     await campspot.save();
-    req.flash('success', 'Created new review!');
+    req.flash('success', "Review successfully posted! Awesome!")
     res.redirect(`/campspots/${campspot._id}`);
 }))
 
-router.delete('/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campspot.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+
+
+
+
+router.delete('/:reviewId', catchAsync(async(req, res) => {
+    const {id, reviewId } = req.params;
+    await Campspot.findByIdAndUpdate(id, {$pull: {reviews: reviewId}}); // review is an array of id's
     await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully deleted review')
+    req.flash('success', "Review successfully Deleted! :(")
     res.redirect(`/campspots/${id}`);
+    // res.send("Delete me")
 }))
 
 module.exports = router;
